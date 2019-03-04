2e3d014d3c2b82c26fb9629ad3d9ef61d369c1e6
Evan Higgins <evdhiggins@gmail.com>
2019-03-04 10:19:58 -0600

    Remove built files

--- built/automateThis/factories/nightmare.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Nightmare = require("nightmare");
-exports.default = () => {
-    return new Nightmare({
-        show: process.env.MISC_DISPLAY_WINDOW === "T",
-        waitTimeout: 20000,
-    });
-};
--- built/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Automator_class_1 = require("./automateThis/classes/Automator.class");
-module.exports = Automator_class_1.default;
