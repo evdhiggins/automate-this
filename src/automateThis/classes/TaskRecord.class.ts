d4defb212c9340d93ba20e3135605841010a6e74
Evan Higgins <evdhiggins@gmail.com>
2019-03-21 07:53:26 -0500

    Only attempt to update changed columns

--- src/automateThis/classes/TaskRecord.class.ts
+++ src/automateThis/classes/TaskRecord.class.ts
@@ -3,11 +3,13 @@ import Airtable = require("Airtable");
 export default class {
   private id: string;
   private fields: Airtable.RecordData;
+  private updatedFields: Airtable.RecordData;
   public isUpdated: boolean = false;
 
   constructor(record: Airtable.Record) {
     this.id = record.getId();
     this.fields = record.fields;
+    this.updatedFields = {};
   }
 
   public getId(): string {
@@ -21,10 +23,18 @@ export default class {
     return Object.assign({}, this.fields);
   }
 
+  /**
+   * Return a shallow copy of record fields
+   */
+  public getUpdateFields(): Airtable.RecordData {
+    return Object.assign({}, this.updatedFields);
+  }
+
   public getUpdate(obj: this) {
     const func = (newFields: Airtable.RecordData) => {
       this.isUpdated = true;
       this.fields = Object.assign({}, this.fields, newFields);
+      this.updatedFields = Object.assign({}, this.updatedFields, newFields);
     };
     func.bind(obj);
     return func;
